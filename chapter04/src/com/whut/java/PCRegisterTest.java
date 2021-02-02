fe65d27bf72f0bb197a60e05294c575d4b31073d
liuxiao <liuxiao2103@qq.com>
2021-02-02 18:56:10 +0800

    提交

--- chapter04/src/com/whut/java/PCRegisterTest.java
+++ chapter04/src/com/whut/java/PCRegisterTest.java
@@ -1,4 +1,14 @@
 package com.whut.java;
 
 public class PCRegisterTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+
+        String s = "abc";
+        System.out.println(i);
+        System.out.println(k);
+
+    }
 }
