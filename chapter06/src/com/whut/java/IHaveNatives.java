6dcc441be299081f86a757f0a4f3ae112ae78637
liuxiao <liuxiao2103@qq.com>
2021-02-03 09:30:21 +0800

    更新本地方法

---
+++ chapter06/src/com/whut/java/IHaveNatives.java
@@ -0,0 +1,12 @@
+package com.whut.java;
+
+public class IHaveNatives {
+    public native void Native1(int x);
+
+    public native static long Native2();
+
+    private native synchronized float Native3(Object o);
+
+    native void Native4(int[] ary) throws Exception;
+
+}
